3d1d4546f1628614a18ab7dd558cfe79a5a7fcd4
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-11-13 14:34:47 -0500

    impl addition for vec3

--- tracer/src/vec3.rs
+++ tracer/src/vec3.rs
@@ -1,5 +1,41 @@
-struct Vec3(f32, f32, f32);
+use std::ops;
+
+type Unit = f32;
+
+#[derive(PartialEq)]
+struct Vec3(Unit, Unit, Unit);
 
 impl Vec3 {
+    fn x(&self) -> Unit {
+        self.0
+    } 
+    fn y(&self) -> Unit {
+        self.1
+    }
+    fn z(&self) ->Unit {
+        self.2
+    }
+}
+impl ops::Add for Vec3 {
+    type Output = Self;
 
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(
+            self.x() + rhs.x(),
+            self.y() + rhs.y(),
+            self.z() + rhs.z(),
+            )
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let v1 = Vec3(5.0, 6.0, 7.0);
+        let v2 = Vec3(2.0, 3.0, 4.0);
+        let v3 = Vec3(7.0, 9.0, 11.0);
+        assert!(v1 + v2 == v3);
+    }
 }
